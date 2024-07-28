86bf33e7f4885eb040967eba060dbf2ea0b79092
Mykyta <161162416+ZozuliaMykyta@users.noreply.github.com>
2024-07-28 19:26:42 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  createHashRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Layout from "./components/Layout";
 import About from "./pages/About";
@@ -6,20 +11,19 @@ import Services from "./pages/Services";
 import Teams from "./pages/Teams";
 import ContactUs from "./pages/ContactUs";
 
+const router = createHashRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Homepage />} />
+      <Route path="about" element={<About />} />
+      <Route path="services" element={<Services />} />
+      <Route path="teams" element={<Teams />} />
+      <Route path="contactus" element={<ContactUs />} />
+    </Route>
+  )
+);
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Homepage />} />
-          <Route path="about" element={<About />} />
-          <Route path="services" element={<Services />} />
-          <Route path="teams" element={<Teams />} />
-          <Route path="contactus" element={<ContactUs />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
